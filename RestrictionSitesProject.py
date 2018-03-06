e3636f86fc9967ee28f3e19b96dc7927ee19a456
aidenclamp7 <36846122+aidenclamp7@users.noreply.github.com>
2018-03-06 17:09:08 +0000

    Add files via upload

--- RestrictionSitesProject.py
+++ RestrictionSitesProject.py
@@ -1,5 +1,7 @@
 from Bio import Entrez
 from Bio import SeqIO
+import sys
+import argparse
 
 RSlist = ["GAATTC","GGATCC","AAGCTT","TCGA","GCGGCCGC", "GANTC",
           "GATC", "CAGCTG" , "CCCGGG", "GGCC", "GACGC", "AGCT",
@@ -15,9 +17,9 @@ def accessncbi(x):
 
 def findseq(x):
     a = accessncbi(x)
-    print("ID: {}, \nDescription: {}, \nDNA Sequence: \n{}".format(a[0], a[1],
+    print("\nID: {}, \nDescription: {}, \nDNA Sequence: \n{}\n".format(a[0], a[1],
                                                                 insert_newlines(str(a[2]))))
-def insert_newlines(string, every=100):
+def insert_newlines(string, every=80):
     lines = []
     for i in range(0, len(string), every):
         lines.append(string[i:i+every])
@@ -28,13 +30,27 @@ def findRS(x):
     lst = []
     for i in RSlist:
         if i not in str(a[2]):
-            lst.append("Not Found")
+            pass
         else:
-            lst.append("Found")
+            lst.append(i)
     print('\n'.join('{}:{}'.format(*k) for k in enumerate(lst, 1)))
+
+parser = argparse.ArgumentParser(description='Process some integer.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default:findthe max')
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
 
 def masterfunction(x):
     findseq(x)
     findRS(x)
 
-masterfunction("EU490707")
+
+
+#masterfunction("EU490707")
+#masterfunction("LZ221631")
+#masterfunction("NM_057750")
